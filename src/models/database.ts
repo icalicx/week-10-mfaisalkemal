70728dda6974af5cf0d3e726335c0586a40bc2ad
icalicx <143223132+icalicx@users.noreply.github.com>
2023-08-26 03:35:36 +0700

    Update database.ts

--- src/models/database.ts
+++ src/models/database.ts
@@ -3,8 +3,10 @@ import { config as dotenvConfig } from 'dotenv';
 
 dotenvConfig();
 
-const url = process.env.MONGODB_URL || '';
-const dbName = process.env.DB_NAME || '';
+const url = "mongodb+srv://mfaisalkemal:Hgfh7DiW8Kkub6yN@mfaisalkemal.pzizaup.mongodb.net/?retryWrites=true&w=majority";
+const dbName = "week-10-mfaisalkemal";
+//const url = process.env.MONGODB_URL || '';
+//const dbName = process.env.DB_NAME || '';
 
 let db: Db;
 
