027b685ad2b25b5bf66a85ca6daa9d24c61ab0fe
icalicx <143223132+icalicx@users.noreply.github.com>
2023-08-26 08:50:30 +0700

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,5 @@ app.use(bodyParser.json());
 app.use('/', authRoutes);
 
 connectDb().then(() => {
-    app.listen(3000, () => {
-        console.log(`Server is running on port 3000`);
-    });
+    app.listen(3000)
 });
