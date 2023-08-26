b5ece951efb3dce06ab5b61f8ac67d31540a073c
icalicx <143223132+icalicx@users.noreply.github.com>
2023-08-26 11:53:05 +0700

    Add files via upload

--- dist/models/database.js
+++ dist/models/database.js
@@ -4,7 +4,9 @@ exports.getDb = exports.connectDb = void 0;
 const mongodb_1 = require("mongodb");
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
-const url = "mongodb+srv://mfaisalkemal:Hgfh7DiW8Kkub6yN@mfaisalkemal.pzizaup.mongodb.net/?retryWrites=true&w=majority";
+// const url = "mongodb+srv://mfaisalkemal:Hgfh7DiW8Kkub6yN@mfaisalkemal.pzizaup.mongodb.net/?retryWrites=true&w=majority";
+// const dbName = "week-10-mfaisalkemal";
+const url = process.env.MONGODB_URL || '';
 const dbName = "week-10-mfaisalkemal";
 let db;
 const connectDb = async () => {
